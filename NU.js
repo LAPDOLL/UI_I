2e1395e6819239314e1b37f0d8ec80a57f69a3ee
GANIYU ABDULLAHI <99150380+LAPDOLL@users.noreply.github.com>
2023-04-16 20:21:51 +0100

    Add files via upload

---
+++ NU.js
@@ -0,0 +1,29 @@
+let my_name = prompt("TYPE YOUR NAME:")
+console.log("HELLO, " + my_name);
+
+
+// SUM OF 1-100
+let total = 0, count = 1;
+while (count <= 100)
+{
+total += count;
+count += 1;
+}
+console.log(total);
+
+
+
+// INPUT AND ADD FIVE NUMBERS
+let first_number = parseInt (prompt("Type the number:"));
+
+let second_number = parseInt (prompt("Type the number:"));
+
+let third_number = parseInt(prompt("Type the number:"));
+
+let fourth_number = parseInt(prompt("Type the number:"));
+
+let fifth_number = parseInt(prompt("Type the number:"));
+    
+var sum = first_number + second_number + third_number + 
+fourth_number + fifth_number;
+console.log(sum);
